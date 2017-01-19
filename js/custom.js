2649f7861fd9c8783415a5f0e3b7de8e7b35d977
minischetti <domminischetti@gmail.com>
2017-01-19 16:01:21 -0500

    Add Home/Login screen, Change body font weight from light to regular, Improve Metro case study copy

---
+++ js/custom.js
@@ -0,0 +1,6 @@
+$(".user").click(function(){
+  $("#home").toggleClass('active');
+  $("h1").toggleClass('active');
+  $(".user-avatar").toggleClass('active');
+  $("span").toggleClass('active');
+});
