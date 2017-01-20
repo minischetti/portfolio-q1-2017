5894004b3789d9f4204075cd0c57f2d9fba3a44c
minischetti <domminischetti@gmail.com>
2017-01-20 14:27:49 -0500

    Refactor, Add YALI case study image, Improve work page layout, Change Dribbble work hover style, Add JS landing page sessionStorage

--- js/custom.js
+++ js/custom.js
@@ -3,4 +3,10 @@ $(".user").click(function(){
   $("h1").toggleClass('active');
   $(".user-avatar").toggleClass('active');
   $("span").toggleClass('active');
+
+  sessionStorage.setItem("selectedProfile", "yes");
 });
+
+if (sessionStorage.getItem("selectedProfile") === "yes") {
+  $('#home').css('display', 'none');
+}
