be65dbab357037548b0895e39c1328b77000e32b
minischetti <domminischetti@gmail.com>
2017-01-22 12:39:23 -0500

    Refactor, Update page titles, Add experimental nav scroll style, Play animations after user select or automatically if sessionStorage = yes

--- js/custom.js
+++ js/custom.js
@@ -1,12 +1,21 @@
 $(".user").click(function(){
-  $("#home").toggleClass('active');
-  $("h1").toggleClass('active');
-  $(".user-avatar").toggleClass('active');
-  $("span").toggleClass('active');
+  $("#home, h1, .user-avatar, span").addClass('active');
+  $('.achievement, .skill, .star').addClass('animate');
 
   sessionStorage.setItem("selectedProfile", "yes");
 });
 
 if (sessionStorage.getItem("selectedProfile") === "yes") {
   $('#home').css('display', 'none');
+  $('.achievement, .skill, .star').addClass('animate');
 }
+/*
+$(window).scroll(function() {
+    if ($(this).scrollTop() > 80) { // this refers to window
+        $('nav').addClass('scroll')
+    }
+    else {
+      $('nav').removeClass('scroll')
+    }
+});
+*/
