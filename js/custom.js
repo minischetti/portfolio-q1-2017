388d3f397c7a3f4382d8d05ff4a38a581db35db5
minischetti <domminischetti@gmail.com>
2017-01-22 22:54:17 -0500

    Hide overflow with profile page open, Stop iOS scrolling of hidden content with profile page open, Improve mobile styles

--- js/custom.js
+++ js/custom.js
@@ -1,14 +1,29 @@
 $(".user").click(function(){
   $("#home, h1, .user-avatar, span").addClass('active');
   $('.achievement, .skill, .star').addClass('animate');
+  $('body').removeClass('noScroll');
+
 
   sessionStorage.setItem("selectedProfile", "yes");
 });
 
-if (sessionStorage.getItem("selectedProfile") === "yes") {
-  $('#home').css('display', 'none');
-  $('.achievement, .skill, .star').addClass('animate');
-}
+//Stop iOS devices from scrolling the page when the menu is open
+$(function stopScroll(){
+var fixed = document.getElementById('home');
+fixed.addEventListener('touchmove', function(e) {
+        e.preventDefault();
+}, false);
+});
+
+$(function checkProfile(){
+  if (sessionStorage.getItem("selectedProfile") === "yes") {
+    $('#home').css('display', 'none');
+    $('.achievement, .skill, .star').addClass('animate');
+  }
+  else {
+    $('body').addClass('noScroll');
+  }
+});
 
 $(window).scroll(function() {
     if ($(this).scrollTop() > 80) { // this refers to window
