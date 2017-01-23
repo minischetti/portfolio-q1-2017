5ff4c8bf2dbc9d65fe04393575607f0ed608784e
minischetti <domminischetti@gmail.com>
2017-01-23 15:58:10 -0500

    Add particles to homepage background, Refactor CSS

--- js/custom.js
+++ js/custom.js
@@ -1,28 +1,36 @@
 $(".user").click(function(){
   $("#home, h1, .user-avatar, span").addClass('active');
-  $('.achievement, .skill, .star').addClass('animate');
+  $('.achievement, .skill, .star, .title span').addClass('animate');
   $('body').removeClass('noScroll');
-
-
   sessionStorage.setItem("selectedProfile", "yes");
-});
-
-//Stop iOS devices from scrolling the page when the menu is open
-$(function stopScroll(){
-var fixed = document.getElementById('home');
-fixed.addEventListener('touchmove', function(e) {
-        e.preventDefault();
-}, false);
 });
 
 $(function checkProfile(){
   if (sessionStorage.getItem("selectedProfile") === "yes") {
     $('#home').css('display', 'none');
-    $('.achievement, .skill, .star').addClass('animate');
+    $('.achievement, .skill, .star, .title span').addClass('animate');
   }
   else {
     $('body').addClass('noScroll');
   }
+});
+
+// If opening case study first, ignore profile select on back
+if (window.location.href.indexOf("metro") > -1) {
+  $('body').removeClass('noScroll');
+  sessionStorage.setItem("selectedProfile", "yes");
+}
+if (window.location.href.indexOf("yali") > -1) {
+  $('body').removeClass('noScroll');
+  sessionStorage.setItem("selectedProfile", "yes");
+}
+
+//Stop iOS devices from scrolling the page when the menu is open
+$(function stopScroll(){
+  var fixed = document.getElementById('home');
+  fixed.addEventListener('touchmove', function(e) {
+    e.preventDefault();
+  }, false);
 });
 
 $(window).scroll(function() {
